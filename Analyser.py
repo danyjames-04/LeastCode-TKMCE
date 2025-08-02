948ac1285f2710ec854b2680ce54058d1fe4a6aa
Dany James <danydjm04@gmail.com>
2025-08-02 07:12:43 +0530

    Update Analyser.py

--- Analyser.py
+++ Analyser.py
@@ -73,7 +73,8 @@ def index():
     return render_template("test.html", sum=meme_result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # use PORT from Render, default to 5000
+    app.run(host='0.0.0.0', port=port)
 
 
 
